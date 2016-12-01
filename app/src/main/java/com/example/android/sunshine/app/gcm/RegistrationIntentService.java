9fd5b4025277b0a8b42e22accd819310b4f14f2a
Daniel <cook348@gmail.com>
2016-11-30 20:09:47 -0500

    chore: Clean up extraneous todos

--- app/src/main/java/com/example/android/sunshine/app/gcm/RegistrationIntentService.java
+++ app/src/main/java/com/example/android/sunshine/app/gcm/RegistrationIntentService.java
@@ -20,7 +20,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.example.android.sunshine.app.MainActivity;
 import com.example.android.sunshine.app.R;
@@ -47,7 +46,7 @@ public class RegistrationIntentService extends IntentService {
                 // are local.
                 InstanceID instanceID = InstanceID.getInstance(this);
 
-                // TODO: gcm_default sender ID comes from the API console
+                // gcm_default sender ID comes from the API console
                 String senderId = getString(R.string.gcm_defaultSenderId);
                 if ( senderId.length() != 0 ) {
                     String token = instanceID.getToken(senderId,
